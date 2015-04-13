d5fb66e2acfbbe8635087559d259ff9650cfdefc
Gregory Klein <gregoryhklein@gmail.com>
2015-04-13 01:36:22 -0400

    Using child vc for alarm options

---
+++ Modularm/AlarmConfigurationController.swift
@@ -0,0 +1,19 @@
+//
+//  AlarmConfigurationController.swift
+//  Modularm
+//
+//  Created by Gregory Klein on 4/13/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+import UIKit
+
+class AlarmConfigurationController: UIViewController
+{
+   private var navigationBarIsHidden = false
+
+   override func viewDidLoad()
+   {
+      super.viewDidLoad()
+   }
+}
---
+++ Modularm/AlarmOptionsController.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmOptionsController.swift
+//  Modularm
+//
+//  Created by Gregory Klein on 4/13/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+import UIKit
+
+class AlarmOptionsController: UIViewController
+{
+   override func viewDidLoad()
+   {
+      super.viewDidLoad()
+   }
+}
