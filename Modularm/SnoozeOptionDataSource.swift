2cf5e5366f98119c99a4e1938568e7a3c7ecea9a
Gregory Klein <gregoryhklein@gmail.com>
2015-04-19 15:43:02 -0400

    Added file for snooze option data source

---
+++ Modularm/SnoozeOptionDataSource.swift
@@ -0,0 +1,13 @@
+//
+//  SnoozeOptionDataSource.swift
+//  Modularm
+//
+//  Created by Gregory Klein on 4/19/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+import UIKit
+
+class SnoozeOptionDataSource: NSObject {
+   
+}
