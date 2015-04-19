025b6c901845dd7286741312355a8e3a86b6026c
Gregory Klein <gregoryhklein@gmail.com>
2015-04-19 12:59:29 -0400

    Added extension for UIImage

---
+++ UIImage+Additions.swift
@@ -0,0 +1,9 @@
+//
+//  UIImage+Additions.swift
+//  Modularm
+//
+//  Created by Gregory Klein on 4/19/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+import Foundation
