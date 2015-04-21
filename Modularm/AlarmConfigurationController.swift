9d5eb66bf9d006cbf353a8c76382b9b3e249ac4f
Gregory Klein <gregoryhklein@gmail.com>
2015-04-21 00:14:00 -0400

    Observing keyboard show/hide notification

--- Modularm/AlarmConfigurationController.swift
+++ Modularm/AlarmConfigurationController.swift
@@ -10,10 +10,41 @@ import UIKit
 
 class AlarmConfigurationController: UIViewController
 {
+   @IBOutlet weak var alarmOptionsControllerBottomVerticalSpaceConstraint: NSLayoutConstraint!
    private var navigationBarIsHidden = false
 
    override func viewDidLoad()
    {
       super.viewDidLoad()
+      NSNotificationCenter.defaultCenter().addObserver(self, selector: "moveAlarmOptionsControllerUpForNotification:", name: UIKeyboardWillShowNotification, object: nil)
+      NSNotificationCenter.defaultCenter().addObserver(self, selector: "moveAlarmOptionsControllerDownForNotification:", name: UIKeyboardWillHideNotification, object: nil)
+   }
+   
+   func moveAlarmOptionsControllerUpForNotification(notification: NSNotification)
+   {
+      if let info = notification.userInfo
+      {
+         let keyboardBoundsValue = info[UIKeyboardFrameEndUserInfoKey] as! NSValue
+         let keyboardBounds = keyboardBoundsValue.CGRectValue()
+         let height = CGRectGetHeight(keyboardBounds)
+         
+         UIView.animateWithDuration(0.25, animations: { () -> Void in
+            self.alarmOptionsControllerBottomVerticalSpaceConstraint.constant = height
+            self.view.layoutIfNeeded()
+         })
+      }
+      println("notification: \(notification)")
+   }
+   
+   func moveAlarmOptionsControllerDownForNotification(notification: NSNotification)
+   {
+      if let info = notification.userInfo
+      {
+         UIView.animateWithDuration(0.25, animations: { () -> Void in
+            self.alarmOptionsControllerBottomVerticalSpaceConstraint.constant = 0
+            self.view.layoutIfNeeded()
+         })
+      }
+      println("notification: \(notification)")
    }
 }
