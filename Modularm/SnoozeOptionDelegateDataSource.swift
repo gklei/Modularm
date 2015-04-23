f86a0751fe79713f8e43c8c407b09fcb696c7a25
Gregory Klein <gregoryhklein@gmail.com>
2015-04-22 21:17:49 -0400

    Big refactor -- turned snooze settings into small state machine

--- Modularm/SnoozeOptionDelegateDataSource.swift
+++ Modularm/SnoozeOptionDelegateDataSource.swift
@@ -8,66 +8,128 @@
 
 import UIKit
 
+protocol SnoozeOptionSettingStateDelegate
+{
+   var cellLabelDictionary: [Int : Array<String>]? {get set}
+   func transitionToState(state: SnoozeOptionSettingState)
+}
+
 class SnoozeOptionDelegateDataSource: AlarmOptionDelegateDataSource
 {
-   // MARK - Instance Variables
-   let firstMenuTitles = ["Snooze", "Regular button", "Big button", "Shake your phone"]
-   let secondMenuTitles = ["5 minutes", "10 minutes", "15 minutes", "20 minutes"]
-   var isShowingFirstMenu: Bool {
-      didSet {
-         if (self.isShowingFirstMenu)
-         {
-            self.settingsControllerDelegate.updateSetOptionButtonClosure(nil)
-            self.settingsControllerDelegate.resetSetOptionButtonTitle()
-         }
-         else
-         {
-            self.settingsControllerDelegate.updateSetOptionButtonTitle("Set snooze time")
-            self.settingsControllerDelegate.updateSetOptionButtonClosure({ () -> () in
-               self.isShowingFirstMenu = true
-               self.tableView.reloadData()
-            })
-         }
-      }
-   }
+   var state: SnoozeOptionSettingState?
 
    // MARK: - Init
    override init(tableView: UITableView, delegate: AlarmOptionSettingsControllerProtocol)
    {
-      self.isShowingFirstMenu = true
       super.init(tableView: tableView, delegate: delegate)
-      self.cellLabelDictionary = [0 : firstMenuTitles]
+      self.state = SnoozeOptionSettingButtonState(delegate: self)
       self.option = .Snooze
    }
-}
-
-// MARK: - UITableView Data Source
-extension SnoozeOptionDelegateDataSource: UITableViewDataSource
-{
+   
+   
+   // MARK: - UITableView Data Source
    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
    {
-      self.cellLabelDictionary = self.isShowingFirstMenu ? [0 : firstMenuTitles] : [0 : secondMenuTitles]
       let cell = super.tableView(tableView, cellForRowAtIndexPath: indexPath)
-      
-      if indexPath.row == 0
-      {
-         cell.accessoryType = self.isShowingFirstMenu ? .DisclosureIndicator : .None
-         cell.selectionStyle = self.isShowingFirstMenu ? .Default : .None
-      }
+      self.state?.configureCell(cell, forRowAtIndexPath: indexPath)
       
       return cell
    }
-}
-
-// MARK: - UITableView Delegate
-extension SnoozeOptionDelegateDataSource: UITableViewDelegate
-{
+   
+   // MARK: - UITableView Delegate
    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
    {
-      if self.isShowingFirstMenu && indexPath.row == 0
+      self.state?.tableView(tableView, didSelectRowAtIndexPath: indexPath)
+   }
+}
+
+extension SnoozeOptionDelegateDataSource: SnoozeOptionSettingStateDelegate
+{
+   func transitionToState(state: SnoozeOptionSettingState)
+   {
+      if state is SnoozeOptionSettingButtonState
       {
-         self.isShowingFirstMenu = false
-         self.tableView.reloadData()
+         self.settingsControllerDelegate.updateSetOptionButtonClosure(nil)
+         self.settingsControllerDelegate.resetSetOptionButtonTitle()
       }
+      else if state is SnoozeOptionSettingTimeState
+      {
+         self.settingsControllerDelegate.updateSetOptionButtonTitle("Set snooze time")
+         self.settingsControllerDelegate.updateSetOptionButtonClosure({ () -> () in
+            
+            let snoozeOptionSettingButtonState = SnoozeOptionSettingButtonState(delegate: self)
+            self.transitionToState(snoozeOptionSettingButtonState)
+            self.tableView.reloadData()
+         })
+      }
+      
+      self.state = state;
+      self.tableView.reloadData()
+   }
+}
+
+class SnoozeOptionSettingState
+{
+   var delegate: SnoozeOptionSettingStateDelegate
+   init(delegate: SnoozeOptionSettingStateDelegate)
+   {
+      self.delegate = delegate
+   }
+   
+   func configureCell(cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath)
+   {
+   }
+   
+   func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
+   {
+   }
+   
+   func cellSelectedAtIndexPath(indexPath: NSIndexPath)
+   {
+   }
+}
+
+class SnoozeOptionSettingButtonState: SnoozeOptionSettingState
+{
+   let snoozeTimeMenuRowIndex = 0
+   override init(delegate: SnoozeOptionSettingStateDelegate)
+   {
+      super.init(delegate: delegate)
+      self.delegate.cellLabelDictionary = [0 : ["Snooze", "Regular button", "Big button", "Shake your phone"]]
+   }
+   
+   override func configureCell(cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath)
+   {
+      if indexPath.row == self.snoozeTimeMenuRowIndex
+      {
+         cell.accessoryType = .DisclosureIndicator
+         cell.selectionStyle = .Default
+      }
+   }
+   
+   override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
+   {
+      if indexPath.row == self.snoozeTimeMenuRowIndex
+      {
+         let snoozeOptionSettingTimeState = SnoozeOptionSettingTimeState(delegate: self.delegate)
+         self.delegate.transitionToState(snoozeOptionSettingTimeState)
+      }
+   }
+}
+
+class SnoozeOptionSettingTimeState: SnoozeOptionSettingState
+{
+   override init(delegate: SnoozeOptionSettingStateDelegate)
+   {
+      super.init(delegate: delegate)
+      self.delegate.cellLabelDictionary = [0 : ["5 minutes", "10 minutes", "15 minutes", "20 minutes"]]
+   }
+   
+   override func configureCell(cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath)
+   {
+   }
+   
+   override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
+   {
    }
 }
