74955928f39020a3f38637f76e53cd538af58730
Gregory Klein <gregoryhklein@gmail.com>
2015-04-29 01:20:41 -0400

    Added init methods for NSAttributedString

--- NSAttributedString+Additions.swift
+++ NSAttributedString+Additions.swift
@@ -6,8 +6,19 @@
 //  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 extension NSAttributedString
 {
+   convenience init(text: String)
+   {
+      let titleAttrs = [NSFontAttributeName : UIFont(name: "HelveticaNeue-Light", size: 19)!]
+      self.init(string: text, attributes: titleAttrs)
+   }
+   
+   convenience init(text: String, color: UIColor)
+   {
+      let titleAttrs = [NSFontAttributeName : UIFont(name: "HelveticaNeue-Light", size: 19)!, NSForegroundColorAttributeName : color]
+      self.init(string: text, attributes: titleAttrs)
+   }
 }
