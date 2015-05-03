bc7a296fd806307c91e48dc089cd8760ad74a59b
Gregory Klein <gregoryhklein@gmail.com>
2015-05-03 03:23:25 -0400

    Added convenience init for appending bold text

--- NSAttributedString+Additions.swift
+++ NSAttributedString+Additions.swift
@@ -21,4 +21,16 @@ extension NSAttributedString
       let titleAttrs = [NSFontAttributeName : UIFont(name: "HelveticaNeue-Light", size: 19)!, NSForegroundColorAttributeName : color]
       self.init(string: text, attributes: titleAttrs)
    }
+
+   convenience init(text: String, boldText: String)
+   {
+      let titleAttrs = [NSFontAttributeName : UIFont(name: "HelveticaNeue-Light", size: 19)!]
+      let boldTitleAttrs = [NSFontAttributeName : UIFont(name: "HelveticaNeue-Medium", size: 19)!]
+
+      let mutableAttributedString = NSMutableAttributedString(string: text, attributes: titleAttrs)
+      let boldAttributedString = NSAttributedString(string: " \(boldText)", attributes: boldTitleAttrs)
+
+      mutableAttributedString.appendAttributedString(boldAttributedString)
+      self.init(attributedString: mutableAttributedString)
+   }
 }
