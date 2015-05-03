ba6322bfc8b2d89ad093c9075feb6ebefa162993
Gregory Klein <gregoryhklein@gmail.com>
2015-05-03 13:40:20 -0400

    Started working on Repeat model

--- Repeat.swift
+++ Repeat.swift
@@ -9,8 +9,29 @@
 import Foundation
 import CoreData
 
+enum RepeatDay
+{
+   case Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+}
+
+@objc(Repeat)
 class Repeat: NSManagedObject
 {
-    @NSManaged var dayArray: AnyObject
-    @NSManaged var alarm: Alarm
+   private var mutableDaySet = Set<RepeatDay>()
+   
+   @NSManaged var dayArray: AnyObject
+   @NSManaged var alarm: Alarm
+   
+   func addDay(day: RepeatDay)
+   {
+      if !self.mutableDaySet.contains(day)
+      {
+         self.mutableDaySet.insert(day)
+      }
+   }
+   
+   func removeDay(day: RepeatDay)
+   {
+      self.mutableDaySet.remove(day)
+   }
 }
