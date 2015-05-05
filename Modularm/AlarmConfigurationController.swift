71467c1b14aac019e6caa8e9f5cb2c81858ce154
Gregory Klein <gregoryhklein@gmail.com>
2015-05-05 00:19:17 -0400

    Added outlet to set bar button item...
    Added methods to setup a temporary alarm and then save it if the set
    button is pressed

--- Modularm/AlarmConfigurationController.swift
+++ Modularm/AlarmConfigurationController.swift
@@ -12,6 +12,7 @@ import CoreData
 class AlarmConfigurationController: UIViewController
 {
    // MARK: - Instance Variables
+   @IBOutlet weak var setBarButtonItem: UIBarButtonItem!
    @IBOutlet weak var alarmOptionsHeightConstraint: NSLayoutConstraint!
    @IBOutlet weak var alarmOptionsControllerBottomVerticalSpaceConstraint: NSLayoutConstraint!
    @IBOutlet weak var segmentedControl: UISegmentedControl!
@@ -23,44 +24,13 @@ class AlarmConfigurationController: UIViewController
    override func viewDidLoad()
    {
       super.viewDidLoad()
-      NSNotificationCenter.defaultCenter().addObserver(self, selector: "moveAlarmOptionsControllerUpForNotification:", name: UIKeyboardWillShowNotification, object: nil)
-      NSNotificationCenter.defaultCenter().addObserver(self, selector: "moveAlarmOptionsControllerDownForNotification:", name: UIKeyboardWillHideNotification, object: nil)
+      self.setupKeboardNotifications()
    }
    
    override func viewWillAppear(animated: Bool)
    {
       super.viewWillAppear(animated);
-
-      self.segmentedControl.selectedSegmentIndex = 1
-      self.segmentedControl.enabled = false
-      self.segmentedControl.userInteractionEnabled = false
-   }
-
-   // MARK: - Public
-   func moveAlarmOptionsControllerUpForNotification(notification: NSNotification)
-   {
-      if let info = notification.userInfo
-      {
-         let keyboardBoundsValue = info[UIKeyboardFrameEndUserInfoKey] as! NSValue
-         let keyboardBounds = keyboardBoundsValue.CGRectValue()
-         let height = CGRectGetHeight(keyboardBounds)
-
-         UIView.animateWithDuration(0.25, animations: {() -> Void in
-            self.alarmOptionsControllerBottomVerticalSpaceConstraint.constant = height
-            self.view.layoutIfNeeded()
-         })
-      }
-   }
-
-   func moveAlarmOptionsControllerDownForNotification(notification: NSNotification)
-   {
-      if let info = notification.userInfo
-      {
-         UIView.animateWithDuration(0.25, animations: {() -> Void in
-            self.alarmOptionsControllerBottomVerticalSpaceConstraint.constant = 0
-            self.view.layoutIfNeeded()
-         })
-      }
+      self.setupSegmentedControl()
    }
    
    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?)
@@ -72,6 +42,70 @@ class AlarmConfigurationController: UIViewController
       else if segue.identifier == "alarmOptionsControllerSegue"
       {
          self.alarmOptionsController = segue.destinationViewController.childViewControllers![0] as? AlarmOptionsController
+      }
+   }
+}
+
+extension AlarmConfigurationController
+{
+   private func setupKeboardNotifications()
+   {
+      NSNotificationCenter.defaultCenter().addObserver(self, selector: "moveAlarmOptionsControllerUpForNotification:", name: UIKeyboardWillShowNotification, object: nil)
+      NSNotificationCenter.defaultCenter().addObserver(self, selector: "moveAlarmOptionsControllerDownForNotification:", name: UIKeyboardWillHideNotification, object: nil)
+   }
+   
+   private func setupSegmentedControl()
+   {
+      self.segmentedControl.selectedSegmentIndex = 1
+      self.segmentedControl.enabled = false
+      self.segmentedControl.userInteractionEnabled = false
+   }
+}
+
+extension AlarmConfigurationController
+{
+   @IBAction func setButtonPressed()
+   {
+      if let alarm = self.alarm
+      {
+         CoreDataStack.defaultStack.managedObjectContext!.insertObject(alarm)
+         CoreDataStack.defaultStack.saveContext()
+      }
+      
+      self.navigationController?.popViewControllerAnimated(true)
+   }
+   
+   // This method creates a temporary alarm by inserting into an nil context
+   func createNewAlarm()
+   {
+      let coreDataStack = CoreDataStack.defaultStack
+      let description = NSEntityDescription.entityForName("Alarm", inManagedObjectContext: coreDataStack.managedObjectContext!)
+      self.alarm = NSManagedObject(entity: description!, insertIntoManagedObjectContext: nil) as? Alarm
+   }
+   
+   func moveAlarmOptionsControllerUpForNotification(notification: NSNotification)
+   {
+      if let info = notification.userInfo
+      {
+         let keyboardBoundsValue = info[UIKeyboardFrameEndUserInfoKey] as! NSValue
+         let keyboardBounds = keyboardBoundsValue.CGRectValue()
+         let height = CGRectGetHeight(keyboardBounds)
+         
+         UIView.animateWithDuration(0.25, animations: {() -> Void in
+            self.alarmOptionsControllerBottomVerticalSpaceConstraint.constant = height
+            self.view.layoutIfNeeded()
+         })
+      }
+   }
+   
+   func moveAlarmOptionsControllerDownForNotification(notification: NSNotification)
+   {
+      if let info = notification.userInfo
+      {
+         UIView.animateWithDuration(0.25, animations: {() -> Void in
+            self.alarmOptionsControllerBottomVerticalSpaceConstraint.constant = 0
+            self.view.layoutIfNeeded()
+         })
       }
    }
 }
