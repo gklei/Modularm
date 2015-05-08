27c158e6505afc923f6127042c1487e9e509d60e
Gregory Klein <gregoryhklein@gmail.com>
2015-05-08 17:33:49 -0400

    Added class for providing icons

---
+++ Modularm/AlarmOptionsImageProvider.swift
@@ -0,0 +1,9 @@
+//
+//  AlarmOptionsImageProvider.swift
+//  Modularm
+//
+//  Created by Klein, Greg on 5/8/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+import Foundation
