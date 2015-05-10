d5cc0388af0b4b1cbd97ecc3860ef01d1742ee1d
Gregory Klein <gregoryhklein@gmail.com>
2015-05-10 11:59:48 -0400

    Changed name of delegate/datasource protocol

---
+++ Modularm/OptionSettingsControllerBase.swift
@@ -0,0 +1,35 @@
+//
+//  OptionSettingsControllerBase.swift
+//  Modularm
+//
+//  Created by Klein, Greg on 5/10/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+import UIKit
+
+class OptionSettingsControllerBase: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
