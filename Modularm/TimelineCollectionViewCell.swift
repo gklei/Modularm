de16ed61bec83d28ecb919537f509d77be0450ed
Gregory Klein <gregoryhklein@gmail.com>
2015-05-12 13:39:53 -0400

    Added custom collection view cell class

---
+++ Modularm/TimelineCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  TimelineCollectionViewCell.swift
+//  Modularm
+//
+//  Created by Klein, Greg on 5/12/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+import UIKit
+
+class TimelineCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
