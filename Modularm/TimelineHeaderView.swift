15edf61a5d088d09529a9718e3597f741cafec6f
Gregory Klein <gregoryhklein@gmail.com>
2015-05-17 13:44:09 -0400

    Added timeline header view

---
+++ Modularm/TimelineHeaderView.swift
@@ -0,0 +1,13 @@
+//
+//  TimelineHeaderView.swift
+//  Modularm
+//
+//  Created by Gregory Klein on 5/17/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+import UIKit
+
+class TimelineHeaderView: UICollectionReusableView
+{
+}
