fb1b2c10143fca01453ab60ef3d3daf900c2cdaf
Gregory Klein <gregoryhklein@gmail.com>
2015-06-12 01:07:47 -0700

    Added informative label to alarm preview

--- Modularm/AlarmPreviewViewController.swift
+++ Modularm/AlarmPreviewViewController.swift
@@ -13,6 +13,7 @@ class AlarmPreviewViewController: UIViewController
    // MARK: - Properties
    @IBOutlet weak var hourLabel: UILabel!
    @IBOutlet weak var minuteLabel: UILabel!
+   @IBOutlet weak var informativeTimeLabel: UILabel!
    
    private weak var alarm: Alarm?
    
@@ -22,6 +23,7 @@ class AlarmPreviewViewController: UIViewController
       super.viewDidLoad()
       self.view.backgroundColor = UIColor.clearColor()
       self.updateHourAndMinuteLabelsWithAlarm(self.alarm)
+      self.updateInformativeTimeLabel()
    }
 
    // MARK: Public
@@ -45,5 +47,37 @@ class AlarmPreviewViewController: UIViewController
       hourInt = hourInt == 0 ? 12 : hourInt
       self.hourLabel.text = hourInt <= 9 ? "0\(hourInt)" : "\(hourInt)"
       self.minuteLabel.text = minute <= 9 ? "0\(minute)" : "\(minute)"
+   }
+   
+   func updateInformativeTimeLabel()
+   {
+      if let alarmDate = self.alarm?.fireDate
+      {
+         let currentDate = NSDate()
+         
+         let currentHourMinute = (currentDate.hour, currentDate.minute)
+         let dateHourMinute = (alarmDate.hour, alarmDate.minute)
+         
+         var hour = dateHourMinute.0 - currentHourMinute.0
+         if dateHourMinute.0 < currentHourMinute.0 {
+            hour += 24
+         }
+         
+         var minute = dateHourMinute.1 - currentHourMinute.1
+         if dateHourMinute.1 < currentHourMinute.1 {
+            minute += 60
+            hour -= 1
+         }
+         
+         if hour == 0 && minute == 0 {
+            hour = 24
+         }
+         
+         if hour < 0 {
+            hour = 23
+         }
+         
+         self.informativeTimeLabel.text = "\(hour) hours and \(minute) minutes"
+      }
    }
 }
