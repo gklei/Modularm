e5175f2418b45e8ee413659d6f032cfeb476d5f9
Gregory Klein <gregoryhklein@gmail.com>
2015-06-21 22:16:01 -0400

    Added code for getting notified when the minute changes

--- Modularm/AppDelegate.swift
+++ Modularm/AppDelegate.swift
@@ -13,10 +13,12 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate
 {
    var window: UIWindow?
+   var minuteChangedNotificationTimer: NSTimer?
 
    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool
    {
       // Override point for customization after application launch.
+      self.startTimerForMinuteChangedNotification()
       return true
    }
 
@@ -24,22 +26,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate
    {
       // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
       // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+      self.stopTimerForMinuteChangedNotification()
    }
 
    func applicationDidEnterBackground(application: UIApplication)
    {
       // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
       // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+      self.stopTimerForMinuteChangedNotification()
    }
 
    func applicationWillEnterForeground(application: UIApplication)
    {
       // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+      self.startTimerForMinuteChangedNotification()
    }
 
    func applicationDidBecomeActive(application: UIApplication)
    {
       // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+      self.startTimerForMinuteChangedNotification()
    }
 
    func applicationWillTerminate(application: UIApplication)
@@ -49,6 +55,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate
 
       let defaultStack = CoreDataStack.defaultStack
       defaultStack.saveContext()
+   }
+   
+   private func startTimerForMinuteChangedNotification()
+   {
+      if self.minuteChangedNotificationTimer == nil
+      {
+         let calendar = NSCalendar.autoupdatingCurrentCalendar()
+         let components = calendar.components(.CalendarUnitSecond, fromDate: NSDate())
+         let currentSecond = components.second
+         
+         let fireDate = NSDate().dateByAddingTimeInterval(NSTimeInterval(60 - currentSecond + 1))
+         self.minuteChangedNotificationTimer = NSTimer(fireDate: fireDate, interval: 60, target: self, selector: Selector("minuteChanged:"), userInfo: nil, repeats: true)
+         
+         NSRunLoop.mainRunLoop().addTimer(self.minuteChangedNotificationTimer!, forMode: NSDefaultRunLoopMode)
+      }
+   }
+   
+   private func stopTimerForMinuteChangedNotification()
+   {
+      self.minuteChangedNotificationTimer?.invalidate()
+      self.minuteChangedNotificationTimer = nil
+   }
+   
+   func minuteChanged(timer: NSTimer)
+   {
+      println("minute changed!")
    }
 }
 
